d618892a4211fbd6182e219c5cac296efdb80924
tarundangeti <tarun.dangeti2@gmail.com>
2025-07-04 02:14:28 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+
+package com.example.employee.controller;
+
+import com.example.employee.model.Employee;
+import com.example.employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+}
